000c5d1db661dfa5bb0693a0492de100bb49b339
JosueTovar05 <josuej.tovar@urosario.edu.co>
2025-04-05 12:36:50 -0500

    DS

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ document.getElementById('patientForm').addEventListener('submit', async function
     };
 
     try {
-        const response = await fetch('https://hl7-fhir-ehrjosue.onrender.com/Patient', {
+        const response = await fetch('https://hl7-fhir-ehrjosue.onrender.com/fhir/patient', { // <== Ajusta según tu backend
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
